b27f7218759bd43ecb9573f054a059a34dbadc5c
Jesus Vega <chuyspc@gmail.com>
2017-11-12 15:08:25 -0800

    Create Input Module

---
+++ test/Should_Throw_Error_If_Receipt_Cannot_Be_Read.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect;
+var assert = require('assert');
+var input_parser = require('./input_parser.js');
+
+describe('Read File', function() {
+  var file_path = '../receipts/input_01.txt';
+  var sample_receipt = `1 book at 12.49
+1 music CD at 14.99
+1 chocolate bar at 0.85`;
+    it('Should Read Correct File', function() {
+      var file = input_parser.load_doc(file_path);
+      assert.equal(file, false);
+    });
+});
