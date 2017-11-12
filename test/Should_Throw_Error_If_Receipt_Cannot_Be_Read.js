ad832f0af7aef26aa95e818991338b9d8d233c4a
Jesus Vega <chuyspc@gmail.com>
2017-11-12 15:42:44 -0800

    Move Input Parser

--- test/Should_Throw_Error_If_Receipt_Cannot_Be_Read.js
+++ test/Should_Throw_Error_If_Receipt_Cannot_Be_Read.js
@@ -1,14 +1,14 @@
 var expect = require('chai').expect;
 var assert = require('assert');
-var input_parser = require('./input_parser.js');
+var input_parser = require('../modules/input_parser').input_parser;
 
 describe('Read File', function() {
-  var file_path = '../receipts/input_01.txt';
+  var file_path = './receipts/input_01.txt';
   var sample_receipt = `1 book at 12.49
 1 music CD at 14.99
 1 chocolate bar at 0.85`;
-    it('Should Read Correct File', function() {
+    it('Can Read Correct File', function() {
       var file = input_parser.load_doc(file_path);
-      assert.equal(file, false);
+      assert.equal(file, sample_receipt);
     });
 });
