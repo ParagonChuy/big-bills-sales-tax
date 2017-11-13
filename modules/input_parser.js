ecf22153064918a7f0e725fb7c93fcc8d654d29d
Jesus Vega <chuyspc@gmail.com>
2017-11-13 04:24:37 -0800

    Doc to JSON

--- modules/input_parser.js
+++ modules/input_parser.js
@@ -3,8 +3,38 @@ var fs = require('fs');
 var input_parser = (function() {
   return {
     load_doc: function(file_path) {
-      // Loads File from file_path and return contents as a string
       return file_contents = fs.readFileSync(file_path, 'utf-8');
+    },
+
+    doc_to_json: function(doc) {
+      var arr = this.raw_doc_to_array(doc);
+      arr = this.filter_empty_objects(arr);
+
+      var mapped = arr.map(function(x) {
+        var amount_price = x.match(/\d+\.\d+|\d+\b|\d+(?=\w)/g);
+        var amount = amount_price[0];
+        var price = amount_price[1];
+
+        var item = x.split(" at ");
+        item = item[0].replace(/[0-9]+/,'').trim();
+
+        var obj = {
+          amount: amount,
+          item: item,
+          tax_rate: 0,
+          price: price,
+        }
+        return obj;
+      });
+
+      return mapped;
+    },
+
+
+    filter_empty_objects: function(arr){
+      return arr.filter(function(e) {
+        return e;
+      });
     },
 
     raw_doc_to_array: function(doc) {
@@ -14,6 +44,7 @@ var input_parser = (function() {
     retrieve_and_ready: function(file_path) {
       var doc = this.load_doc(file_path);
       var arr = this.raw_doc_to_array(doc);
+      this.doc_to_json(doc);
       return arr;
     },
 
--- test/Should_Throw_Error_If_Receipt_Cannot_Be_Read.js
+++ test/Should_Throw_Error_If_Receipt_Cannot_Be_Read.js
@@ -4,11 +4,24 @@ var input_parser = require('../modules/input_parser').input_parser;
 
 describe('Read File', function() {
   var file_path = './receipts/input_01.txt';
+  var file = input_parser.load_doc(file_path);
   var sample_receipt = `1 book at 12.49
 1 music CD at 14.99
-1 chocolate bar at 0.85`;
-    it('Can Read Correct File', function() {
-      var file = input_parser.load_doc(file_path);
+1 chocolate bar at 0.85
+`;
+
+    it('Input Parser loads text file', function() {
+      var valid_string = (typeof file != 'undefined' && typeof file === 'string');
+      assert.equal(valid_string, true);
+    });
+
+    it('Input Parser has correct contents', function() {
       assert.equal(file, sample_receipt);
+    });
+
+    it('Input Parser parses into JSON', function() {
+      var arr = input_parser.doc_to_json(file);
+      var valid_json = (typeof arr === 'object');
+      assert.equal(valid_json , true);
     });
 });
