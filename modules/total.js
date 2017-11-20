4a12f31b3b500fa2bdae48f31da83c3df7bc886d
Jesus Vega <chuyspc@gmail.com>
2017-11-20 15:11:09 -0800

    Add realine-sync package

--- modules/total.js
+++ modules/total.js
@@ -3,29 +3,19 @@ const calculate_tax = require('../modules/calculate_tax').calculate_taconst
 
 const total = (function(){
   return {
+    getSalesTax: function(arr){
+      return arr.reduce((total, curr) => total + curr.sales_tax());
+    },
     getTotal: function(arr){
-      let [price_grand, sales_grand, total_grand ] = [0,0,0];
-
-      let prices = arr.forEach(curr => price_grand += curr.price);
-      let total = arr.forEach(curr => total_grand += curr.total());
-      let sales_tax = arr.forEach(curr => sales_grand += curr.sales_tax());
-      let move = {
-        price: price_grand.toFixed(2),
-        sales_tax: sales_grand.toFixed(2),
-        total: total_grand.toFixed(2)
-      }
-      return move;
+      return arr.reduce((total, curr) => total + curr.total());
     },
     finalize_tab: function(arr){
-      let output = "";
-      arr.forEach(curr => {
-        output += `${curr.amount} ${curr.item} at ${curr.price}\n`
-      });
       console.log("RECEIPT\n");
-      console.log(output);
-      let move = this.getTotal(arr);
-      console.log(`Sales Tax: ${move.sales_tax}`);
-      console.log(`Total: ${move.total}`);
+      let output = arr.forEach(curr => console.log(`${curr.amount} ${curr.item} at ${curr.price}`));
+      let total = this.getTotal(arr);
+      let sales_tax = this.getSalesTax(arr);
+      console.log(`\nSales Tax: ${sales_tax}`);
+      console.log(`Total: ${total}`);
     }
   }
 })();
